a3ac0a0a9e5ce3a0cbfa1e7795bc5142804321ef
Jokeseph <josephofilii@gmail.com>
2022-03-25 09:12:46 +0100

    React App for movies using omdb api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './components/Header/Header';
 import { BrowserRouter } from 'react-router-dom';
-// import Trending from './components/movies/Trending';
+import Trending from './components/movies/Trending';
 import Latest from './components/movies/Latest';
 import React, { useState, useEffect } from 'react'
 
@@ -75,21 +75,7 @@ function App() {
     {/* Trending Section Starts */}
 
     <section className='trending-section'>
-    <div className='trending-container'>
-
-      <div className='trending-text'> 
-        Trending Movies
-      </div>
-        <div className='slideshow'>
-            <div className='movie'>
-
-                <MovieList/>
-
-            </div>
-            
-        </div>
-      
-      </div>
+      <Trending MovieList = {MovieList}/>
       
     </section>
 
